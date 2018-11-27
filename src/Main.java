7703f33339638bd1d18a357be43c8ea44586d88f
simonbrage <simonbrage@live.dk>
2018-11-27 13:22:06 +0100

    test2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello bitch World!");
+        System.out.println("Hello bitch bitch World!");
     }
 }
